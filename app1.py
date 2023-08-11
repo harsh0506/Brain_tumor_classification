bca57e33209eca625e2e9a9a00bee5b58251e1b5
harsh0506 <joshiharsh0506@gmail.com>
2023-08-11 19:32:52 +0530

    created webfront

--- app1.py
+++ app1.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify ,render_template
+import asyncio
+from functools import partial
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -8,45 +10,46 @@ app1 = Flask(__name__)
 # Load the trained model
 model = tf.keras.models.load_model("./models/braintumor_model_gpu_1.h5")
 
-@app1.route("/",methods=["GET"])
+@app1.route("/", methods=["GET"])
 def htmlfile():
     return render_template("index.html")
-  
-@app1.route('/predict', methods=['POST'])
-def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-    
+
+async def predict_image(file):
     try:
-        CLASS_TYPES = [ 'glioma','meningioma', 'notumor', 'pituitary']
+        CLASS_TYPES = ['glioma', 'meningioma', 'notumor', 'pituitary']
         image = Image.open(file)
         image = image.resize((150, 150))
         image_array = np.array(image, dtype=np.float32)
         image_array = np.expand_dims(image_array, axis=0)
         image_array /= 255.0  # Normalize the image
-        
+
         predictions = model.predict(image_array)
         predicted_class_index = np.argmax(predictions)
         predicted_class_label = CLASS_TYPES[predicted_class_index]
-        
+
         response = {
             'class_label': predicted_class_label,
             'class_index': predicted_class_index,
             'probabilities': predictions.tolist()[0],
-            "condition" : CLASS_TYPES[predicted_class_index]
+            "condition": CLASS_TYPES[predicted_class_index]
         }
         print(response)
-        
-        return jsonify(response)
-    
-    except Exception as e:
-        return jsonify({'error': str(e)})
+        return f"the condition is {response['condition']}"
 
+    except Exception as e:
+        return {'error': str(e)}
+
+@app1.route('/predict', methods=['POST'])
+def handle_predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    return jsonify(asyncio.run(predict_image(file)))
 
 if __name__ == '__main__':
     app1.run(debug=True)
